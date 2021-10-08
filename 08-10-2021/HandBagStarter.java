b5f4253a65eec7611f9bf98464c1e107e7aa8cad
shashankcoder27 <59994206+shashankcoder27@users.noreply.github.com>
2021-10-09 01:50:26 +0530

    constructor and instance

---
+++ 08-10-2021/HandBagStarter.java
@@ -0,0 +1,17 @@
+class Starter
+{
+	public static void main(String[] Rao)
+	{
+		new HandBag("wildCraft",1.5f); //1
+		new HandBag("leather"); //2
+		new HandBag(500.50f); //3
+		new HandBag("medium","leather"); //4
+		new HandBag(1.5f,"wildCraft"); //5
+		new HandBag(1.5f,500.50f); //6
+		new HandBag("medium","leather",1.5f); //7
+		new HandBag(1.5f,"wildCraft","medium");// 8
+		new HandBag(1.5f,"wildCraft",500.50f); //9
+		new HandBag(1.5f,"wildCraft",500.50f,"leather"); //10
+		
+	}
+}
