ce88cf4509af8b54c1e2b1df7e3353d7e4d05449
shashankcoder27 <59994206+shashankcoder27@users.noreply.github.com>
2021-11-17 10:54:33 +0530

    object methods

---
+++ 17-11-2021/NewspaperStarter.java
@@ -0,0 +1,26 @@
+import com.xworkz.inheritance.objectmethods.NewsPaper;
+
+public class Newspaper {
+
+	public static void main(String[] args) {
+		NewsPaper newsPaper = new NewsPaper();
+		newsPaper.setName("VijayaKarnataka");
+		newsPaper.setLanguage("Kannada");
+		newsPaper.setSince(1999);
+
+		NewsPaper newsPaper1 = new NewsPaper();
+		newsPaper1.setName("Vijayavani");
+		newsPaper1.setLanguage("Kannada");
+		newsPaper1.setSince(1999);
+
+		if (newsPaper == newsPaper1) {
+			System.out.println("both pointing to same memory");
+		}
+
+		boolean same = newsPaper.equals(newsPaper1);// TRUE
+		{
+			System.out.println(same);
+		}
+	}
+
+}
